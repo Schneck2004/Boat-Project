02a2f48dcb735bd860fdff436690ed594f68ece4
schneck2004 <guilhermeschneck@gmail.com>
2026-01-30 14:18:14 +0100

    Initial commit 	new file:   Main/Icons/Arduino-logo.svg 	new file:   Main/Icons/Arduino.svg 	new file:   Main/Icons/Battery-dashboard.svg 	new file:   Main/Icons/Battery.svg 	new file:   Main/Icons/Boat-complete.svg 	new file:   Main/Icons/Boat.svg 	new file:   Main/Icons/ESC-icon.svg 	new file:   Main/Icons/ESC.svg 	new file:   Main/Icons/Green-telemetry-icon.svg 	new file:   Main/Icons/Humidity-sensor-icon.svg 	new file:   Main/Icons/Humidity-sensor.svg 	new file:   Main/Icons/Lightning.svg 	new file:   Main/Icons/Motor-icon.svg 	new file:   Main/Icons/Motor.svg 	new file:   Main/Icons/Red-telemetry-icon.svg 	new file:   Main/Icons/Servo-icon.svg 	new file:   Main/Icons/Servo.svg 	new file:   Main/Icons/Yellow-telemetry-icon.svg 	new file:   Main/Images/Background.svg 	new file:   Main/Scripts/home.js 	new file:   Main/Styles/home.css 	new file:   Main/index.html

---
+++ Main/Scripts/home.js
@@ -0,0 +1,11 @@
+function resetRudder() {
+  document.getElementById("rudder").value = 0
+}
+
+function rightRudder() {
+    document.getElementById("rudder").value = 20
+}
+
+function leftRudder() {
+    document.getElementById("rudder").value = -20
+}
